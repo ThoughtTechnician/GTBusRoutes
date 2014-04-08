9d5ec8164ed76338ad3c168da9f1e6a03db4e020
Connor Reeder <connor.reeder@ymail.com>
2014-04-07 22:58:18 -0400

    First Polyline

--- src/com/thoughttechnician/gtbusroutes/RouteMapActivity.java
+++ src/com/thoughttechnician/gtbusroutes/RouteMapActivity.java
@@ -7,16 +7,17 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnFocusChangeListener;
-import android.view.View.OnHoverListener;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class RouteMapActivity extends ActionBarActivity {
 	private static final LatLng ATLANTA = new LatLng(33.755,-84.39);
@@ -36,17 +37,29 @@ public class RouteMapActivity extends ActionBarActivity {
 			fm.beginTransaction().add(R.id.mapFragmentContainer, mapFragment).commit();
 		}
 		GoogleMap map = ((SupportMapFragment)mapFragment).getMap();
-
-	}
-	public void onCheckboxClicked(View view) {
-		GoogleMap map = ((SupportMapFragment) getSupportFragmentManager()
-				.findFragmentById(R.id.mapFragmentContainer)).getMap();
 		map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 			@Override
 			public void onMapClick(LatLng arg0) {
 				Log.d(TAG, "LatLng: " + arg0);
 			}
 		});
+		PolylineOptions polyOptions = new PolylineOptions()
+			.add(new LatLng(33.781779953549396,-84.40672282129526))
+			.add(new LatLng(33.768557503688875,-84.39630880951881));
+		Polyline polyline = map.addPolyline(polyOptions);
+		CheckBox redCheckBox = (CheckBox) findViewById(R.id.checkbox_red);
+		redCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				
+			}
+		});
+	}
+	public void onCheckboxClicked(View view) {
+		GoogleMap map = ((SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.mapFragmentContainer)).getMap();
+		
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
